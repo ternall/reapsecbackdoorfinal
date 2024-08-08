126ffbc1f849231f02df9bb1681bbc9c452224c8
ternall <143479753+ternall@users.noreply.github.com>
2024-08-07 20:51:45 -0700

    Update setup.ps1
    I HATE THIS!!!!
    still trying to shorten the run dialogue down to 259...

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,5 @@
-# is python installed?
+# is python installed
 if (-Not (Get-Command python -ErrorAction SilentlyContinue)) {
-    # Download and install Python
     Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe' -OutFile 'C:\Users\Public\python-installer.exe'
     Start-Process 'C:\Users\Public\python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait
     Remove-Item -Path 'C:\Users\Public\python-installer.exe'
